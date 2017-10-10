0a5e05fb4011d634b0442d97c94acd8fcb35c8fb
Sanjin Dumancic <sanjin.dumancic@yahoo.com>
2017-10-10 21:29:36 +0200

    project lombok

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -5,6 +5,7 @@ import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.CategoryRepository;
 import guru.springframework.repositories.UnitOfMeasureRepository;
 import guru.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,7 @@ import java.util.Optional;
 /**
  * Created by Sanjin on 2.10.2017..
  */
+@Slf4j
 @Controller
 public class IndexController {
 
@@ -27,7 +29,7 @@ public class IndexController {
 
     @RequestMapping(value = {"/","","/index"})
     public String getIndexPage(Model model){
-
+        log.debug("Get index page");
         model.addAttribute("recipes",recipeService.getRecipes());
 
         return "index";
--- src/main/java/guru/springframework/domain/Notes.java
+++ src/main/java/guru/springframework/domain/Notes.java
@@ -1,7 +1,11 @@
 package guru.springframework.domain;
 
+import lombok.*;
+
 import javax.persistence.*;
 
+@Data
+@EqualsAndHashCode(exclude = {"recipe"})
 @Entity
 public class Notes {
 
@@ -15,27 +19,8 @@ public class Notes {
     @Lob
     private String recipeNotes;
 
-    public Long getId() {
-        return id;
+    protected boolean canEqual(Object other) {
+        return other instanceof Notes;
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Recipe getRecipe() {
-        return recipe;
-    }
-
-    public void setRecipe(Recipe recipe) {
-        this.recipe = recipe;
-    }
-
-    public String getRecipeNotes() {
-        return recipeNotes;
-    }
-
-    public void setRecipeNotes(String recipeNotes) {
-        this.recipeNotes = recipeNotes;
-    }
 }
